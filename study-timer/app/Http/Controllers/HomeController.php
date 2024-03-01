f3f47809a09dd1596df5c37b7a0ffd84014aa92f
Aathish R Viswam <aathishrv@gmail.com>
2024-03-01 14:20:44 +0530

    authentication

---
+++ study-timer/app/Http/Controllers/HomeController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class HomeController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('home.register');
+    }
+    public function authenticate(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        $email = $request->input('email');
+        $password = $request->input('password');
+
+        if (Auth::attempt(['email' => $email, 'password' => $password])) {
+            $newuser = User::where('email', $email)->first();
+            Auth::login($newuser);
+            return redirect('/book');
+        } else {
+            return back()->withErrors(['Invalid credentials!']);
+        }
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate(['name' => 'required']);
+        $request->validate(['email' => 'required']);
+        $request->validate(['password' => 'required']);
+        try {
+            $newuser = User::create([
+                'name' => $request->input('name'),
+                'email' => $request->input('email'),
+                'password' => Hash::make($request->input('password')),
+            ]);
+            return redirect('/login');
+        } catch (\Exception $e) {
+            // return back()->withErrors(['Invalid credentials!']);
+            return redirect()->route('signup')->withErrors(['Invalid credentials!']);
+        }
+    }
+}
--- study-timer/routes/web.php
+++ study-timer/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,4 +18,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::view("index", 'home.index');
+<<<<<<< Updated upstream
 Route::view("clock", 'home.clock.clock');
+=======
+Route::view("/login", 'home.login');
+Route::get('/signup', [HomeController::class, 'index'])->name('home.register');
+
+Route::post('/signup', [HomeController::class, 'register'])->name('signup');
+
+Route::post('/authenticate', [HomeController::class, 'authenticate']);
+>>>>>>> Stashed changes
