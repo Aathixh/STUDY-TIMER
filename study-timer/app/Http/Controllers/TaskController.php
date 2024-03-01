e954e3380d526e90c18dbff03960a6518dbba687
Aathish R Viswam <aathishrv@gmail.com>
2024-03-01 21:39:44 +0530

    todo half way

---
+++ study-timer/app/Http/Controllers/TaskController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TasksModel;
+use Illuminate\Http\Request;
+use App\Models\Tasks;
+
+class TaskController extends Controller
+{
+    //
+    // public function list(Request $request)
+    // {
+    //     $request->validate(['tasks' => 'required']);
+
+    //     try {
+    //         $newTask = Tasks::create([
+    //             $tasks = $request->input('tasks')
+    //         ]);
+    //         return redirect()->route('home.dash');
+    //     } catch (\Exception $e) {
+    //         return back()->withErrors(['Invalid credentials!']);
+    //         // return redirect()->route('/login')->withErrors(['Invalid credentials!']);
+    //     }
+
+    // }
+    public function list(Request $request)
+    {
+        $request->validate(['tasks' => 'required']);
+
+        try {
+            $newTask = Tasks::create([
+                'tasks' => $request->input('tasks')
+            ]);
+
+            return redirect()->route('home.dash');
+        } catch (\Exception $e) {
+            return back()->withErrors(['Invalid credentials!']);
+        }
+    }
+
+}
--- study-timer/resources/views/layouts/todo.blade.php
+++ study-timer/resources/views/layouts/todo.blade.php
@@ -1,5 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-    this is to-do
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <form method ='post' action="/todo"> 
+            @csrf
+                @method('post')
+                @if ($errors->any())
+                <ul style="color: red;">
+                    @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+                @endif
+                <input type='text' name="tasks" placeholder='Enter the Task'>
+            <button type='submit'>Submit</button>
+        </form>
+
+    </body>
+    </html>
 @endsection
